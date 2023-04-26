7836274dc6574f6dcb28b6a69afc33a7d302dc1a
pbullock08 <126013657+pbullock08@users.noreply.github.com>
2023-04-26 12:32:53 -0400

    finalized grabbing the data for the current location and temp

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,6 +52,8 @@ cityInput.addEventListener('click', function (event) {
     cityInput.value = '';
 })
 
+
+
 // fetch geo data 
 function weatherFetch() {
     var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput.value}&appid=6d8c8c2517262ec75ca50bfee4f15b76`
@@ -78,16 +80,23 @@ function weatherFetch() {
                     console.log(data.list[0].wind.speed); //current wind speed
                     console.log(data.list[0].main.humidity); //current humidity 
 
-                    var currentHeader = document.querySelector('.current h4')
+                    var currentHeader = document.querySelector('.current h4');
+                    currentHeader.textContent = data.city.name + ' ' + dayjs(data.list[0].dt_txt).format('M/DD/YYYY') + ' ' + 'http://openweathermap.org/img/w/' + data.list[0].weather[0].icon + '.png';
 
+                    var currentTemp = document.querySelector('.current .l1');
+                    currentTemp.textContent = 'Temp: ' + ((data.list[0].main.temp - 273.15)*9/5+32).toFixed(2) + '°F';
+
+                    var currentWind = document.querySelector('.current .l2');
+                    currentWind.textContent = 'Wind: ' + data.list[0].wind.speed + ' ' + 'MPH';
+
+                    var currentHumid = document.querySelector('.current .l3');
+                    currentHumid.textContent = 'Humidity: ' + data.list[0].main.humidity + '%';
                     
+                    for (var i = 6; i < data.length; i += 8) {
+                        console.log(data);
 
-
-
-
-                    //for (var i = 0; i < data.length; i++) {
-
-                    //}
+                    }
+                    //6, 14, 22, 30, 38
                 })
         })
 
